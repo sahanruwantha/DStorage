0f52e3b0716eaaa0d67c7fc2747ade0e3245c853
sahanruwantha <sahanr.silva@hotmail.com>
2024-04-14 11:08:33 +0530

    added p2p

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sahanruwantha/DStorage
 
 go 1.16
+
+require (
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/anthdm/foreverstore/p2p"
+)
 
 func main()  {
+	tr := p2p.NewTCPTranport()
+
 	fmt.Println("Ayubowan")
 }
